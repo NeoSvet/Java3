a930ca0168d3067b48efb0e3d1c863c7b6cf0507
Neosvet <neosvet333@gmail.com>
2020-12-24 23:02:13 +0300

    upgraded Tunnel

--- src/ru/neosvet/lesson5/Tunnel.java
+++ src/ru/neosvet/lesson5/Tunnel.java
@@ -1,8 +1,13 @@
 package ru.neosvet.lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
-        this.length = 80;
+    private Semaphore limiter;
+
+    public Tunnel(int length, int limit) {
+        this.length = length;
+        limiter = new Semaphore(limit);
         this.description = "Тоннель " + length + " метров";
     }
 
@@ -11,12 +16,14 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                limiter.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                limiter.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
